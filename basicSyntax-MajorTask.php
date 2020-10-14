5e23358fc55587a1e72227b868eda3c32c6edd26
Ezeche L. Samuel <62204958+LanreSam@users.noreply.github.com>
2020-10-14 15:39:17 -0500

    made changes

--- basicSyntax-MajorTask.php
+++ basicSyntax-MajorTask.php
@@ -16,7 +16,7 @@
     echo "<h3>Lanre Sam</h3>";
 
     //tell me about yourself.
-    echo "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis quos quae voluptate similique? Quidem perferendis odit cumque? 
+    echo "<p>Lorem ipsum dolor sit amet constant, adipisicing elit. Facilis quos quae voluptate similique? Quidem perferendis odit cumque? 
             Fugit quod non veniam labore incidunt, ex suscipit ratione beatae impedit, laborum illo in fugiat ad vero obcaecati debitis nesciunt eveniet, 
             culpa aspernatur? Similique iusto quisquam, voluptate possimus fugiat vero explicabo accusamus velit alias porro rem eos ullam laborum dolorum 
             nostrum veniam ab iure suscipit pariatur excepturi eum animi. Voluptatum ea architecto ab, facere voluptatem, quia dolorum esse repellat accusamus 
