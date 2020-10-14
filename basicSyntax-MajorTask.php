e09c9c2418ba089dde308fee10bdb583f63e6439
Ezeche L. Samuel <62204958+LanreSam@users.noreply.github.com>
2020-10-14 15:38:08 -0500

    Add files via upload

---
+++ basicSyntax-MajorTask.php
@@ -0,0 +1,27 @@
+<?php
+    //====== First major task =======
+
+    
+    /* 
+        Occupation:
+        Gender:
+        Sidehustle Username:
+
+    */
+
+    //about me tag
+    echo "<h1>About Me</h1> <hr/>";
+
+    //your side hustle internship username
+    echo "<h3>Lanre Sam</h3>";
+
+    //tell me about yourself.
+    echo "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis quos quae voluptate similique? Quidem perferendis odit cumque? 
+            Fugit quod non veniam labore incidunt, ex suscipit ratione beatae impedit, laborum illo in fugiat ad vero obcaecati debitis nesciunt eveniet, 
+            culpa aspernatur? Similique iusto quisquam, voluptate possimus fugiat vero explicabo accusamus velit alias porro rem eos ullam laborum dolorum 
+            nostrum veniam ab iure suscipit pariatur excepturi eum animi. Voluptatum ea architecto ab, facere voluptatem, quia dolorum esse repellat accusamus 
+            dolorem sunt inventore aut neque voluptates pariatur repudiandae, 
+            culpa et! Perspiciatis laborum ut tempora quod ullam sapiente, saepe earum, distinctio, aliquid quam repellat!
+        </p>";
+
+?>
